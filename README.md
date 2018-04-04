bf095282318dd2550cb8e6bbbc6649d9257e20e9
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-04-04 15:10:14 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue-Parcel-Pack
+
+## Usage
+```
+git clone https://github.com/tomatoaiu/vue-parcel-pack.git
+cd vue-parcel-pack
+npm i
+npm run start //access http://localhost:1234
+```
---
+++ src/js/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    {{word}}
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            word: "Hello, world",
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
