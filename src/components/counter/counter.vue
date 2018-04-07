bd5031d92875279cf7a0775c2d04d7ecd0560b3f
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-04-08 00:55:56 +0900

    feat: create multi count view by one component vue

--- src/components/counter/counter.vue
+++ src/components/counter/counter.vue
@@ -2,18 +2,21 @@
 <div>
     <counterA></counterA>
     <counterB></counterB>
+    <viewCounter></viewCounter>
 </div>
 </template>
 
 <script>
 import counterA from './a-counter'
 import counterB from './b-counter'
+import viewCounter from './view-counter'
 
 export default {
     name: "counter",
     components: {
         "counterA" : counterA,
-        "counterB" : counterB
+        "counterB" : counterB,
+        "viewCounter" : viewCounter
     }
 }
 </script>
---
+++ src/components/counter/view-counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    A : {{countA}} 
+    B : {{countB}}
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    name: "view-counter",
+    computed: {
+        ...mapGetters({
+            countA: "a/count",
+            countB: "b/count"
+        })
+    }
+}
+</script>
